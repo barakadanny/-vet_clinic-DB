d2a234d6a5b09b79c9cc8607eff3030cc05b3718
barakadanny <53088629+barakadanny@users.noreply.github.com>
2022-10-24 13:21:31 +0300

    Create animal table

--- schema.sql
+++ schema.sql
@@ -1,5 +1,10 @@
 /* Database schema to keep the structure of entire database. */
 
 CREATE TABLE animals (
-    name varchar(100)
+    id integer PRIMARY KEY AUTOINCREMENT,
+    name varchar(100),
+    data_of_birth date,
+    escape_attempts integer,
+    neutered boolean,
+    weight_kg decimal
 );
