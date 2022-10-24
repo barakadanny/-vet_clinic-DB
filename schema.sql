2cc7cd7d87f48a37182c46f5e0c391f29f273277
barakadanny <53088629+barakadanny@users.noreply.github.com>
2022-10-24 15:44:52 +0300

    ID data type

--- schema.sql
+++ schema.sql
@@ -1,7 +1,7 @@
 /* Database schema to keep the structure of entire database. */
 
 CREATE TABLE animals (
-    id integer PRIMARY KEY AUTOINCREMENT,
+    id SERIAL PRIMARY KEY,
     name varchar(100),
     data_of_birth date,
     escape_attempts integer,
