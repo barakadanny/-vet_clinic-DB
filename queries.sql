bc2d0ffcadafb7bc8bdca12667ebcc7f3e5254b6
barakadanny <53088629+barakadanny@users.noreply.github.com>
2022-10-24 13:34:35 +0300

    Queries

--- queries.sql
+++ queries.sql
@@ -1,3 +1,10 @@
 /*Queries that provide answers to the questions from all projects.*/
 
-SELECT * from animals WHERE name = 'Luna';
+SELECT * from animals WHERE name like '%mon';
+SELECT name from animals WHERE date_of_birth between '2016-01-01' and '2019-12-31';
+SELECT name from animals WHERE neutered = 'true' and escape_attempts < 3;
+SELECT data_of_birth from animals WHERE name = 'Agumon' or name = 'Pikachu';
+SELECT name, escape_attempts from animals WHERE weight_kg > 10.4;
+SELECT * from animals WHERE neutered = 'true';
+SELECT * from animals WHERE name != 'Gabumon';
+SELECT * from animals WHERE weight_kg between 10.4 and 17.3;
