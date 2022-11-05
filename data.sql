5c9a36de6ef64e5dd6119dbb67b347edda79e60c
Danny <barakadan421@gmail.com>
2022-11-06 00:18:50 +0300

    Add new species

--- data.sql
+++ data.sql
@@ -23,6 +23,7 @@ INSERT INTO owners (full_name, age) VALUES ('Jodie Whittaker', '38');
 
 INSERT INTO species (name) VALUES ('Pokemon');
 INSERT INTO species (name) VALUES ('Digimon');
+INSERT INTO species (name) VALUES ('Dogen');
 
 UPDATE animals SET species_id = 2 WHERE name LIKE '%mon';
 UPDATE animals SET species_id = 1 WHERE name NOT LIKE '%mon';
